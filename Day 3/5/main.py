fceb3e3855cd970e04f00b5933c0d28856cf5043
Emil Privér <emilpriver@privann.localdomain>
2020-12-03 21:31:51 +0100

    Reorder maps

--- Day 3/5/main.py
+++ Day 3/5/main.py
@@ -1,28 +1,21 @@
-""" Advent of code day 3 part 2 """
+""" Advent of code day 3 part 1 """
 
 with open("input.txt") as text_file:
   lines = list(text_file.readlines())
-  total_tress = 1
-  slopes = [[1,1], [3, 1],[5, 1],[7, 1],[1, 2]]
+  total_tress = 0
+  x = 3
 
-  for slope in slopes:
-    slope_trees = 0
-    x = slope[0]
-    y = slope[1]
+  for (index, line) in enumerate(lines):
+    line = line.strip("\n")
+    if index == 0:
+      continue
 
-    for (index, line) in enumerate(lines[::y]):
-      line = line.strip("\n")
-      if index == 0:
-        continue
+    if line[x] == "#":
+      total_tress += 1
 
-      if line[x] == "#":
-        slope_trees += 1
+    x += 3
 
-      x += slope[0]
-
-      if len(line) <= x:
-        x = x - len(line)
-
-    total_tress *= slope_trees
+    if len(line) <= x:
+      x = x - len(line)
 
   print(total_tress)
